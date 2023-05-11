745011b59ec288698a3978e198fa1d43b97e9ea2
Allen Keettikkal (alk423) <alk423@mail.usask.ca>
2023-05-11 11:34:21 -0600

    Added Parenthesis

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 function addToResult(value) {
-    document.getElementById('result').value += value;
+    // Get the current value of the result field
+    var result = document.getElementById("result").value;
+    
+    // Add the new value to the end of the result
+    result += value;
+    
+    // Update the result field with the new value
+    document.getElementById("result").value = result;
 }
 
 function calculate() {
-    var result = eval(document.getElementById('result').value);
-    document.getElementById('result').value = result;
+    // Get the current value of the result field
+    var result = eval(document.getElementById("result").value);
+
+    // Evaluate the expression using the eval function
+    try {
+        var answer = eval(result);
+        document.getElementById("result").value = answer;
+      } catch (e) {
+        // If the expression is invalid, display an error message
+        document.getElementById("result").value = "Error";
+      }
 }
 
 function clearResult() {
-    document.getElementById('result').value = '';
+    document.getElementById("result").value = "";
 }
